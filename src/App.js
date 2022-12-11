21597035e2ac16eecece7d53144a2bb60a1ddef5
Daniel Interiano <danielstanly@yahoo.es>
2022-12-11 17:49:57 -0600

    helper HttpApi consumo GET

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import CrudApi from './components/CrudApi';
+import CrudApiJsonServer from './components/CrudApiJsonServer';
 import CrudApp from './components/CrudApp';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<CrudApp></CrudApp>
 				<hr />
+				<CrudApiJsonServer></CrudApiJsonServer>
+				<hr />
+				<CrudApi></CrudApi>
+				<hr />
+				<CrudApp></CrudApp>
 			</header>
 		</div>
 	);
---
+++ src/components/CrudApiJsonServer.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { helpHttp } from '../helpers/helpHttp';
+import CrudForm from './CrudForm';
+import CrudTable from './CrudTable';
+
+const CrudApiJsonServer = () => {
+	const [db, setDb] = useState([]);
+	const [dataToEdit, setDataToEdit] = useState(null);
+
+	let api = helpHttp();
+	let url = 'http://localhost:3001/santos';
+
+	useEffect(() => {
+		api.get(url).then((res) => {
+			if (!res.err) {
+				setDb(res);
+			} else {
+				setDb([]);
+			}
+		});
+	}, []);
+
+	const createData = (data) => {
+		data.id = Date.now();
+		setDb([...db, data]);
+	};
+
+	const updateData = (data) => {
+		let newData = db.map((el) => (el.id === data.id ? data : el));
+		setDb(newData);
+	};
+
+	const deleteData = (id) => {
+		let isDelete = window.confirm(`¿Estas seguro de eliminar el registro con el id '${id}'?`);
+
+		if (isDelete) {
+			let newData = db.filter((el) => el.id !== id);
+			setDb(newData);
+		} else {
+			return;
+		}
+	};
+
+	return (
+		<div>
+			<h2>CRUD APP con API JSON-SERVER</h2>
+			<article className="grid-1-2">
+				<CrudForm
+					createData={createData}
+					updateData={updateData}
+					dataToEdit={dataToEdit}
+					setDataToEdit={setDataToEdit}
+				></CrudForm>
+				<CrudTable data={db} setDataToEdit={setDataToEdit} deleteData={deleteData}></CrudTable>
+			</article>
+		</div>
+	);
+};
+
+export default CrudApiJsonServer;
